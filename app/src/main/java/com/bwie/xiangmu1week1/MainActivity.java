058defeca7659f2ab5cc20105a83488cc414f1de
xuchenwei1 <344122741@qq.com>
2019-06-09 19:39:16 +0800

    第三次改动 创建按钮 2019年6月9日19:39:08 徐晨威

--- app/src/main/java/com/bwie/xiangmu1week1/MainActivity.java
+++ app/src/main/java/com/bwie/xiangmu1week1/MainActivity.java
@@ -2,6 +2,8 @@ package com.bwie.xiangmu1week1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //演示
+        TextView textView=new TextView(this,null);
+        //按钮
+        Button button=new Button(this,null);
     }
 }

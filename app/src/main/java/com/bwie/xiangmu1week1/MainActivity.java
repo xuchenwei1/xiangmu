fc363523ee7f407638d46ca8816ac364f7713855
xuchenwei1 <344122741@qq.com>
2019-06-09 19:47:46 +0800

    第一次修改 创建 2019年6月9日19:47:38 徐晨威

--- app/src/main/java/com/bwie/xiangmu1week1/MainActivity.java
+++ app/src/main/java/com/bwie/xiangmu1week1/MainActivity.java
@@ -2,6 +2,7 @@ package com.bwie.xiangmu1week1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //演示
+        TextView textView=new TextView(this,null);
     }
 }
